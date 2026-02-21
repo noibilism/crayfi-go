6d176cc1e061e5684ab77aaaac20750340981269
jamilamasa <jamilamasa@gmail.com>
2026-02-21 15:00:23 +0100

    feat: add new fx endpoints

--- fx.go
+++ fx.go
@@ -1,25 +1,31 @@
 package crayfi
 
+import "fmt"
+
 type FXService struct {
 	client *Client
 }
 
 func (s *FXService) Rates(data interface{}) (interface{}, error) {
-	return s.client.post("/api/v2/merchant/rates", data)
+	return s.client.post("/api/rates", data)
 }
 
 func (s *FXService) RatesByDestination(data interface{}) (interface{}, error) {
-	return s.client.post("/api/v2/merchant/rates/destination", data)
+	return s.client.post("/api/rates/destination", data)
 }
 
 func (s *FXService) Quote(data interface{}) (interface{}, error) {
-	return s.client.post("/api/v2/merchant/quote", data)
+	return s.client.post("/api/quote", data)
 }
 
 func (s *FXService) Convert(data interface{}) (interface{}, error) {
-	return s.client.post("/api/v2/merchant/conversions/convert", data)
+	return s.client.post("/api/conversions", data)
 }
 
 func (s *FXService) Conversions() (interface{}, error) {
-	return s.client.get("/api/v2/merchant/conversions", nil)
+	return s.client.get("/api/conversions", nil)
+}
+
+func (s *FXService) DisputeConversion(conversionID string, data interface{}) (interface{}, error) {
+	return s.client.post(fmt.Sprintf("/api/conversions/%s/dispute", conversionID), data)
 }
